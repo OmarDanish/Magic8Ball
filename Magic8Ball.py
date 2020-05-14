284e58e2c098d3ace2a73845a33baff248b1c6c2
OmarDanish <65194488+OmarDanish@users.noreply.github.com>
2020-05-14 12:32:10 -0500

    Added .lower()

--- Magic8Ball.py
+++ Magic8Ball.py
@@ -1,8 +1,11 @@
+print ("    ")
+\
+
 import  time 
 import random
-i=1
+check=True
 
-while i >= 1:
+while check:
     user_question=input("Ask a question, the Magic 8-Ball will answer:\n")
     \
     print ("The 8-Ball is thinking...\n")
@@ -11,16 +14,15 @@ while i >= 1:
     answers=["Yes", 'No', 'Definitely', 'Definitely not', 'I dont know', 'I was not designed for such a complex question', 'Who knows?', 'For sure', 'Dont get your hopes up']
     
     print(random.choice(answers))
-    
+    \
     time.sleep(2)
     
     play_again=input('Do you want to ask the 8-Ball another question?\n')
+    play_again.lower()
     \
-    if play_again == "Yes" or "yes":
-        i += 1
-    elif play_again == "No" or "no":
-        i = 0
-else:
-     print ('Thank you for using the Magic 8-Ball!')
+    if play_again == "yes":
+        check=True
+    elif play_again == "no":
+        check=False
 
-
+print ('\nThank you for using the Magic 8-Ball!')
